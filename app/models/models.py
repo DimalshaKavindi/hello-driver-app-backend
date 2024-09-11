c4dc07c9be8c1ddeefca775f2ada95ff82829ebf
DimalshaKavindi <dimalshakavindi2020@gmail.com>
2024-09-11 09:05:03 +0530

    commit

--- app/models/models.py
+++ app/models/models.py
@@ -1,29 +1,32 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
 from app.database.database import Base
 
 class Driver(Base):
     __tablename__ = 'drivers'
     
-    driver_id = Column(Integer,primary_key=True, index=True)
+    id = Column(Integer,primary_key=True, index=True)
+    driver_id = Column(Integer, index=True)
     driver_name = Column(String, index=True)
-    vehicle_capacity = Column(float, index=True)
+    vehicle_capacity = Column(Float, index=True)
 
 class Schedule(Base):
     __tablename__ = 'schedules'
     
-    schedule_id = Column(Integer, primary_key=True, index=True)
-    schedule_driver_id = Column(Integer, ForeignKey("drivers.driver_id"))
+    id = Column(Integer,primary_key=True, index=True)
+    schedule_id = Column(Integer, index=True)
     status = Column(String, index=True)
+    schedule_driver_id = Column(Integer, ForeignKey("drivers.id"))
     
 class Orders(Base):
     __tablename__ = 'orders'
     
-    order_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer,primary_key=True, index=True)
+    order_id = Column(Integer, index=True)
     longitude = Column(String, index=True)
     latitude = Column(String, index=True)
-    order_weight = Column(float, index=True)
-    order_driver_id = Column(Integer, ForeignKey("drivers.driver_id"))
-    order_schedule_id = Column(Integer, ForeignKey("schedules.schedule_id"))
+    order_weight = Column(Float, index=True)
+    order_driver_id = Column(Integer, ForeignKey("drivers.id"))
+    order_schedule_id = Column(Integer, ForeignKey("schedules.id"))
     
     
 
